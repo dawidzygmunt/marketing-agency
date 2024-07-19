674817f1a0e4d04d4cb504230f8df189c27dbb19
Dawid <dawid.zygmunt86@gmail.com>
2024-07-19 17:31:59 +0200

    footer

--- app/(dashboard)/components/footer.tsx
+++ app/(dashboard)/components/footer.tsx
@@ -1,28 +1,31 @@
 import { navLinks } from "@/components/nav-links"
 import Link from "next/link"
-import React from "react"
+import Newsletter from "./newsleter"
 
 const Footer = () => {
   return (
-    <div className="bg-black p-10 py-16 flex gap-32">
-      <div>
-        <h3 className="text-2xl">Navigation</h3>
-        <ul className="flex flex-col items-start py-2 text-gray-400">
-          {navLinks.map((link) => (
-            <li key={link.title} className="py-2">
-              <Link href={link.path}>{link.title}</Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+    <div className="bg-black p-10 py-16 flex flex-col">
+      <div className="flex gap-32 ">
+        <div>
+          <h3 className="text-2xl">Nawigacja</h3>
+          <ul className="flex flex-col items-start py-2 text-[#878787]">
+            {navLinks.map((link) => (
+              <li key={link.title} className="py-2">
+                <Link href={link.path}>{link.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
 
-      <div>
-        <h3 className="text-2xl">Info</h3>
-        <ul className="flex flex-col items-start py-2 text-gray-400">
-          <li className="py-2">+48 123 456 789</li>
-          <li className="py-2">info@agency.com</li>
-        </ul>
+        <div>
+          <h3 className="text-2xl">Kontakt</h3>
+          <ul className="flex flex-col items-start py-2 text-[#878787]">
+            <li className="py-2">+48 123 456 789</li>
+            <li className="py-2">info@agency.com</li>
+          </ul>
+        </div>
       </div>
+      <Newsletter />
     </div>
   )
 }
---
+++ app/(dashboard)/components/newsleter.tsx
@@ -0,0 +1,68 @@
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+const FormSchema = z.object({
+  email: z
+    .string()
+    .min(2, {
+      message: "Podaj adres E-mai",
+    })
+    .email({
+      message: "Niepoprawny adres E-mail",
+    }),
+})
+
+const Newsletter = () => {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      email: "",
+    },
+  })
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {}
+  return (
+    <div className="my-7">
+      <h3 className="text-2xl my-4 font-semibold">Zapisz się do newslettera</h3>
+      <p className="text-[#878787] my-7">
+        Nie wysyłamy często wiadomości, nie będziemy cię denerwować{" "}
+      </p>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-2/3 space-y-6"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="example@gmail.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button className="bg-purple text-white mt-5 px-9 py-6" type="submit">
+            Potwierdź
+          </Button>
+        </form>
+      </Form>
+    </div>
+  )
+}
+
+export default Newsletter
